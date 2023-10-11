13840fe18c8e692fe580234255c7a61194846c18
AntonZinchenko32 <zinchenko309@gmail.com>
2023-10-11 22:27:19 +0300

    Видаляє логіку збереження данних в localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,41 +3,16 @@ import { combineReducers } from '@reduxjs/toolkit';
 // Slices
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
-// Persist
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
 
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
-
-const persistConfig = {
-  key: 'root',
-    storage,
-  blacklist: ['filter']
-};
 
 const rootReducer = combineReducers({
   contacts: contactsReducer,
   filter: filterReducer,
 });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  reducer: rootReducer
 });
 
-export const persistor = persistStore(store);
+
